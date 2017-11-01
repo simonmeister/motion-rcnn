fix clipping min sign, much smaller init weights for fully connected motion layer

object_detection/core/box_predictor.py
@@ -455,7 +455,7 @@ class MaskRCNNBoxPredictor(BoxPredictor):
         motion_predictions = slim.fully_connected(
             flattened_image_features,
             self._num_classes * self._num_motion_params,
-            weights_initializer=tf.truncated_normal_initializer(0.0, 0.01),
+            weights_initializer=tf.truncated_normal_initializer(0.0, 0.0001),
             activation_fn=None,
             scope='MotionPredictor')
         instance_motions = tf.reshape(

object_detection/utils/motion_util.py
@@ -6,11 +6,11 @@
 import tensorflow as tf
 
 
-def clip_to_open_interval(x, xmin=-1, xmax=1, eps=1e-08):
-  """Clip absolute value to be strictly less than limits.
+def clip_to_open_interval(x, xmin=-1.0, xmax=1.0, eps=1e-08):
+  """Clip value to be strictly within the limits.
   E.g., the return value with default limits is safe to be
   used inside acos."""
-  return tf.clip_by_value(x, -xmin + eps, xmax - eps)
+  return tf.clip_by_value(x, xmin + eps, xmax - eps)
 
 
 def euler_to_rot(x, y, z, sine_inputs=True):
@@ -106,14 +106,16 @@ def _motion_losses(pred, target):
   d_rot = rot_T @ gt_rot
   #d_rot = gt_rot @ rot_T
   d_trans = gt_trans - trans
-  #d_trans = tf.squeeze(rot_T @ tf.reshape(gt_trans - trans, [-1, 3, 1]),
-  #                     axis=2)
+  d_trans = tf.squeeze(rot_T @ tf.reshape(gt_trans - trans, [-1, 3, 1]),
+                       axis=2)
   d_pivot = gt_pivot - pivot
 
   err_angle = tf.acos(clip_to_open_interval((tf.trace(d_rot) - 1) / 2))
   err_trans = tf.norm(d_trans, axis=1)
   err_pivot = tf.norm(d_pivot, axis=1)
-
+  #err_angle = tf.acos(clip_to_open_interval((tf.trace(rot) - 1) / 2))
+  #err_trans = tf.norm(trans, axis=1)
+  #err_pivot = tf.norm(pivot, axis=1)
   return err_angle, err_trans, err_pivot
 
 

object_detection/utils/np_motion_util.py
@@ -56,7 +56,7 @@ def dense_flow_from_motion(depth, motions, masks, camera_motion,
   for i in range(motions.shape[0]):
     rot = np.reshape(motions[i, :9], [3, 3])
     trans = np.reshape(motions[i, 9:12], [3])
-    pivot = np.reshape(motions[i, 12:], [3])
+    pivot = np.reshape(motions[i, 12:15], [3])
     mask = np.expand_dims(masks[i, :, :], 2)
     P += mask * ((P - pivot).dot(rot.T) + pivot + trans - P)
 
@@ -106,6 +106,9 @@ def _motion_errors(pred, target):
   rot = np.reshape(pred[:, 0:9], [-1, 3, 3])
   trans = pred[:, 9:12]
   pivot = pred[:, 12:15]
+  # x = np.mean(pred[:, 15])
+  # y = np.mean(pred[:, 16])
+  # z = np.mean(pred[:, 17])
 
   gt_rot = np.reshape(target[:, 0:9], [-1, 3, 3])
   gt_trans = target[:, 9:12]
@@ -142,6 +145,9 @@ def _motion_errors(pred, target):
     mean_rel_trans = np.array([0])
 
   error_dict = {
+      # 'meanRotX': x,
+      #     'meanRotY': y,
+      #         'meanRotZ': z,
       'mAngle': mean_angle,
       'mTrans': mean_trans,
       'mPivot': mean_pivot,